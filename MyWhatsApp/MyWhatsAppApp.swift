a9c74d5d20e554a1a50073ff1be6697485b2f6b0
huunghia.nguyen <nghianguyenhuu848@gmail.com>
2024-10-29 21:42:49 +0700

    Installing Firebase SDKs

--- MyWhatsApp/MyWhatsAppApp.swift
+++ MyWhatsApp/MyWhatsAppApp.swift
@@ -6,9 +6,29 @@
 //
 
 import SwiftUI
+import Firebase
+
+/// Install the Firebase Dependencies: File -> Add Package Dependencies -> https://github.com/firebase/firebase-ios-sdk (10.22.1)
+/// FirebaseAuth
+/// FirebaseDatabase
+/// FirebaseDatabaseSwift
+/// FirebaseMessaging
+/// FirebaseStorage
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        
+        return true
+    }
+}
 
 @main
 struct MyWhatsAppApp: App {
+    // register app delegate for Firebase setup
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             MainTabView()
